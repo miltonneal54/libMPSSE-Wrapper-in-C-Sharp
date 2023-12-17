3039b13e5851e5970f3988a51a5232c80be7edb9
Milton Neal <65113973+miltonneal54@users.noreply.github.com>
2023-12-17 12:26:42 +0800

    Add files via upload
    Initial Commit

---
+++ MPSSE_I2C.cs
@@ -0,0 +1,188 @@
+﻿/***************************************************************************
+ *   Copyright (C) 2023 by Milton Neal                                     *
+ *   milton200954@gmail.com                                                *
+ *                                                                         *
+ *   This program is free software: you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation, either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace MPSSENet
+{
+    /// <summary>
+    /// Wrapper class for libMPSSE - I2C.
+    /// </summary>
+    public sealed partial class MPSSE_I2C : MPSSE
+    {
+        /// <summary>
+        /// Constructor for the MPSSE I2C Class.
+        /// </summary>
+        public MPSSE_I2C()
+        { }
+
+        /// <summary>
+        /// Gets the number of channels connected to the host system that can be configured as an I2C master.
+        /// </summary>
+        /// <param name="numberOfChannels">Pointer to unsigned int to stores the number of channels.</param>
+        /// <returns>FT_STATUS value from I2C_GetNumChannels in libMPSSE.DLL</returns>
+        public MPSSE.FT_STATUS I2C_GetNumChannels(ref uint numberOfChannels)
+        {
+            FT_STATUS status = FT_STATUS.FT_OTHER_ERROR;
+
+            status = MPSSE_API.I2C_GetNumChannels(ref numberOfChannels);
+            return status;
+        }
+
+        /// <summary>
+        /// Provides information about the channel referenced by index..
+        /// </summary>
+        /// <param name="index">Index of the channel.</param>
+        /// <param name="deviceInfo">Type class FT_DEVICE_INFO_NODE that contains the device information.</param>
+        /// <returns>FT_STATUS value from I2C_GetChannelInfo in libMPSSE.DLL</returns>
+        public MPSSE.FT_STATUS I2C_GetChannelInfo(uint index, FT_DEVICE_INFO_NODE deviceInfo)
+        {
+            FT_STATUS status = MPSSE.FT_STATUS.FT_OTHER_ERROR;
+            MPSSE_API.FT_DEVICE_LIST_INFO_NODE deviceInfoNode;
+
+            // Get the size of the device info structure and allocate memory on the heap.
+            int size = Marshal.SizeOf<MPSSE_API.FT_DEVICE_LIST_INFO_NODE>();
+            IntPtr channelInfoPointer = Marshal.AllocHGlobal(size);
+
+            status = MPSSE_API.I2C_GetChannelInfo(index, channelInfoPointer);
+            if (status == MPSSE.FT_STATUS.FT_OK)
+            {
+                // Copy the native data to the managed type class
+                deviceInfoNode = Marshal.PtrToStructure<MPSSE_API.FT_DEVICE_LIST_INFO_NODE>(channelInfoPointer);
+                deviceInfo.Flags = deviceInfoNode.Flags;
+                deviceInfo.Type = (MPSSE.FT_DEVICE)deviceInfoNode.Type;
+                deviceInfo.ID = deviceInfoNode.ID;
+                deviceInfo.LocId = deviceInfoNode.LocId;
+                deviceInfo.SerialNumber = GetString(deviceInfoNode.SerialNumber);
+                deviceInfo.Description = GetString(deviceInfoNode.Description);
+                deviceInfo.Handle = deviceInfoNode.Handle;
+            }
+
+            // Free the unmanaged memory.
+            Marshal.FreeHGlobal(channelInfoPointer);
+            return status;
+        }
+
+        /// <summary>
+        /// Opens the indexed channel and provides a handle to it.
+        /// </summary>
+        /// <param name="index">Index of the device to open.</param>
+        /// <returns>FT_STATUS value from I2C_OpenChannel in libMPSSE.DLL</returns>
+        public FT_STATUS I2C_OpenChannel(uint index)
+        {
+            FT_STATUS status = FT_STATUS.FT_OTHER_ERROR;
+
+            status =  MPSSE_API.I2C_OpenChannel(index, ref handle);
+            if(status != FT_STATUS.FT_OK)
+            {
+                handle = IntPtr.Zero;
+            }
+
+            return status;
+        }
+
+        /// <summary>
+        /// Initializes the I2C channel and the communication parameters associated with it.
+        /// </summary>
+        /// <param name="channelConfig">Type class holding the channel configuration data</param>
+        /// <returns>FT_STATUS value from I2C_InitChannel in libMPSSE.DLL</returns>
+        public FT_STATUS I2C_InitChannel(ChannelConfig channelConfig)
+        {
+            FT_STATUS status = FT_STATUS.FT_OTHER_ERROR;
+            MPSSE_API.I2C_CHANNEL_CONFIG config = new MPSSE_API.I2C_CHANNEL_CONFIG();
+
+            if (handle != IntPtr.Zero)
+            {
+                // Copy the managed type class into the native structure.
+                config.ClockRate = channelConfig.ClockRate;
+                config.LatencyTimer = channelConfig.LatencyTimer;
+                config.ConfigOptions = channelConfig.ConfigOptions;
+
+                // Get the size of the channel config structure and allocate memory on the heap.
+                int size = Marshal.SizeOf<MPSSE_API.I2C_CHANNEL_CONFIG>();
+                IntPtr configPointer = Marshal.AllocHGlobal(size);
+                Marshal.StructureToPtr<MPSSE_API.I2C_CHANNEL_CONFIG>(config, configPointer, false);
+
+                status = MPSSE_API.I2C_InitChannel(handle, configPointer);
+
+                // Free the unmanaged memory.
+                Marshal.FreeHGlobal(configPointer);
+            }
+
+            return status;
+        }
+
+        /// <summary>
+        /// Closes the channel.
+        /// </summary>
+        /// <returns>FT_STATUS value from I2C_CloseChannel in libMPSSE.DLL</returns>
+        public FT_STATUS I2C_CloseChannel()
+        {
+            FT_STATUS status = FT_STATUS.FT_DEVICE_NOT_OPENED;
+
+            if(handle != IntPtr.Zero)
+            {
+                status = MPSSE_API.I2C_CloseChannel(handle);
+            }
+
+            handle = IntPtr.Zero;
+            return status;
+        }
+
+        /// <summary>
+        /// Reads the specified number of bytes from an addressed I2C slave.
+        /// </summary>
+        /// <param name="deviceAddress">Address of the I2C slave.</param>
+        /// <param name="sizeToTransfer">Number of bytes to be read.</param>
+        /// <param name="dataBuffer">Pointer to the buffer where data is to be read.</param>
+        /// <param name="sizeTransfered">Pointer to variable containing the number of bytes read.</param>
+        /// <param name="options">Specifies data transfer options.</param>
+        /// <returns>FT_STATUS value from I2C_DeviceRead in libMPSSE.DLL</returns>
+        public FT_STATUS I2C_DeviceRead(uint deviceAddress, uint sizeToTransfer, byte[] dataBuffer, ref uint sizeTransfered, uint options)
+        {
+            FT_STATUS status = FT_STATUS.FT_OTHER_ERROR;
+
+            if(handle != IntPtr.Zero)
+            {
+                status = MPSSE_API.I2C_DeviceRead(handle, deviceAddress, sizeToTransfer, dataBuffer, ref sizeTransfered, options);
+            }
+
+            return status;
+        }
+
+        /// <summary>
+        /// Writes the specified number of bytes to an addressed I2C slave.
+        /// </summary>
+        /// <param name="deviceAddress">Address of the I2C slave.</param>
+        /// <param name="sizeToTransfer">Number of bytes to be written</param>
+        /// <param name="dataBuffer">Pointer to the buffer where data is to be written.</param>
+        /// <param name="sizeTransfered">Pointer to variable containing the number of bytes written.</param>
+        /// <param name="options">Specifies data transfer options.</param>
+        /// <returns>FT_STATUS value from I2C_DeviceWrite in libMPSSE.DLL</returns>
+        public FT_STATUS I2C_DeviceWrite(uint deviceAddress, uint sizeToTransfer, byte[] dataBuffer, ref uint sizeTransfered, uint options)
+        {
+            FT_STATUS status = FT_STATUS.FT_OTHER_ERROR;
+
+            if (handle != IntPtr.Zero)
+            {
+                status = MPSSE_API.I2C_DeviceWrite(handle, deviceAddress, sizeToTransfer, dataBuffer, ref sizeTransfered, options);
+            }
+
+            return status;
+        }
+    }
+}
---
+++ MPSSE_I2C_Options.cs
@@ -0,0 +1,117 @@
+﻿namespace MPSSENet
+{
+    public partial class MPSSE_I2C
+    {
+        /// <summary>
+        /// Type class that holds the parameters used for initializing a channel.
+        /// </summary>
+        public class ChannelConfig
+        {
+            /// <summary>
+            /// Device clock rate.
+            /// </summary>
+            public uint ClockRate;
+
+            /// <summary>
+            /// Latency timer in milliseconds.
+            /// </summary>
+            /// <remarks>FT2232D 2 - 255 milliseconds, 1 - 255 milliseconds for others</remarks>
+            public byte LatencyTimer;
+
+            /// <summary>
+            /// Configuration options.
+            /// </summary>
+            public uint ConfigOptions;
+        }
+
+        /// <summary>
+        /// I2C standard clockrates.
+        /// </summary>
+        /// <remarks>Alternatively a value for a non-standard clock rate may be passed directly.</remarks>
+        public class ClockRate
+        {
+            /// <summary>
+            /// Standard clock mode - 100KHz.
+            /// </summary>
+            public const uint I2C_CLOCK_STANDARD_MODE = 100000;
+
+            /// <summary>
+            /// Fast clock mode - 400KHz.
+            /// </summary>
+            public const uint I2C_CLOCK_FAST_MODE = 400000;
+
+            /// <summary>
+            /// Fast plus clock mode - 1Mhz.
+            /// </summary>
+            public const uint I2C_CLOCK_FAST_MODE_PLUS = 1000000;
+
+            /// <summary>
+            /// High seed clock mode - 3.4MHz
+            /// </summary>
+            public const uint I2C_CLOCK_HIGH_SPEED_MODE = 3400000;
+        }
+
+        /// <summary>
+        /// Configuration option flags.
+        /// </summary>
+        public class ConfigOptions
+        {
+            /// <summary>
+            /// Bit 0 set disables 3 Phase Clocking.
+            /// </summary>
+            /// <remarks>Only availabel for Hi-speed devices (FT232H, FT2232H, FT4232H).</remarks>
+            public const uint I2C_DISABLE_3PHASE_CLOCKING = 0x00000001;
+
+            /// <summary>
+            /// Bit 1 set enables Drive Only Zero.
+            /// </summary>
+            /// <remarks>Available only in a FT232H device.</remarks>
+            public const uint I2C_ENABLE_DRIVE_ONLY_ZERO = 0x00000002;
+
+            // Bits 2 to 31 reserved.
+        }
+
+        /// <summary>
+        /// I2C device transfer option flags.
+        /// </summary>
+        public class TransferOptions
+        {
+            /// <summary>
+            /// If set then a start condition is generated in the I2C bus before the transfer begins.
+            /// </summary>
+            public const uint I2C_TRANSFER_OPTIONS_START_BIT = 0x00000001;
+
+            /// <summary>
+            /// If set then a stop condition is generated in the I2C bus after the transfer ends.
+            /// </summary>
+            public const uint I2C_TRANSFER_OPTIONS_STOP_BIT = 0x00000002;
+
+            /// <summary>
+            /// If set then the function will return when a device nAcks after a byte has been transferred. (Write Option)
+            /// </summary>
+            public const uint I2C_TRANSFER_OPTIONS_BREAK_ON_NACK = 0x00000004;
+
+            /// <summary>
+            /// If set generates a NAK for the last data byte read. (Read Option)
+            /// </summary>
+            public const uint I2C_TRANSFER_OPTIONS_NACK_LAST_BYTE = 0x00000008;
+
+            /// <summary>
+            /// Setting this bit will invoke a multi byte I2C transfer.
+            /// </summary>
+            public const uint I2C_TRANSFER_OPTIONS_FAST_TRANSFER_BYTES = 0x00000010;
+
+            /// <summary>
+            /// Setting this bit would invoke a multi bit transfer.
+            /// </summary>
+            public const uint I2C_TRANSFER_OPTIONS_FAST_TRANSFER_BITS = 0x00000020;
+
+            /// <summary>
+            /// The device address parameter is ignored if this bit is set.
+            /// </summary>
+            public const uint I2C_TRANSFER_OPTIONS_NO_ADDRESS = 0x00000040;
+
+            // Bits 7 to 31 reserved.
+        }
+    }
+}
